f8a199e39d780c63363c8b0de3da10aafc37ff45
karthikeyankuppu <43454404+karthikeyankuppu@users.noreply.github.com>
2020-11-10 11:40:44 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var DATASET_ID = 'ckh4lcauk066d24r8a6dcc06y';
 var DATASETS_BASE = 'https://api.mapbox.com/datasets/v1/karthikeyankuppu/' + DATASET_ID + '/';
-var DATASETS_ACCESS_TOKEN = 'pk.eyJ1Ijoia2FydGhpa2V5YW5rdXBwdTEiLCJhIjoiY2toYmppaGZlMDhqajJyb2d4ZTNsMTB4dCJ9.hhf_23B9XQfe9l2njyiS1g';
+var DATASETS_ACCESS_TOKEN = 'sk.eyJ1Ijoia2FydGhpa2V5YW5rdXBwdTEiLCJhIjoiY2toYmtzM3hxMGIybjJ6bGY5YWM1c3hxMCJ9.eNATQDNAVPZ72U-n3yXByg';
 var PUBLIC_ACCESS_TOKEN = 'pk.eyJ1Ijoia2FydGhpa2V5YW5rdXBwdTEiLCJhIjoiY2toM2Jla3o4MGswODJycGRuNzFzbG9oeCJ9.RWvh-3f8v857LeFnLMp5qg';
 var STYLESHEET = 'mapbox://styles/mapbox/streets-v11';
 var LOAD_INFO_LAYERS = ['chennai-relief-camps', 'chennai-relief-camps-22nov'];
